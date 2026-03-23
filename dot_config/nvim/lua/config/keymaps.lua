44987d190ed71fd77cb0cdda08613bcfe50f1bdc
Hayden Koch <me@haydenkoch.com>
2026-03-23 02:15:48 -0700

    Update .chezmoiignore Add .vscode/settings.json Update .config/aerospace/aerospace.toml Update .config/ghostty/config Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/config/lazy.lua Update .config/nvim/lua/config/options.lua Update .config/tmux/tmux.conf

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Splits (consistent with tmux: | vertical, - horizontal)
+map("n", "<leader>|", "<cmd>vsplit<cr>", { desc = "Split vertical" })
+map("n", "<leader>-", "<cmd>split<cr>", { desc = "Split horizontal" })
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+local opt = vim.opt
+
+-- System clipboard (yank → tmux → macOS clipboard)
+opt.clipboard = "unnamedplus"
+
+-- Keep context around cursor
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+-- Faster CursorHold events (LSP diagnostics, etc.)
+opt.updatetime = 200
+
+-- True color (LazyVim sets this, explicit for tmux interop)
+opt.termguicolors = true
